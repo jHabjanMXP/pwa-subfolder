104a27ebb7cebffa1e0be07e58ac61fbaf6c8973
Jernej Habjan <jhabjan@marinexchange.com>
2022-11-07 11:43:07 +0100

    Initial commit

---
+++ service-worker-assets.js
@@ -0,0 +1,9 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-fvYM8i7Blsw2j\/ZaEUwJX2o\/mWTamnWbTK6V\/jdVpG0=",
+      "url": "styles.css"
+    }
+  ],
+  "version": "qtoNDurP"
+};
